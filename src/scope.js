4c320daf847c0384b424bf0ee68dacae707de2ba
RathaR <ivan.pecharitsa@gmail.com>
2015-04-22 00:33:39 +0300

    prevent unnecessary object iterations

--- src/scope.js
+++ src/scope.js
@@ -265,10 +265,12 @@ Scope.prototype.$destroy = function () {
 
 Scope.prototype.$watchCollection = function (watchFn, listenerFn) {
     var newValue, oldValue;
+    var oldLength;
     var self = this;
     var changeCount = 0;
 
     var internalWatchFn = function (scope) {
+        var newLength, key;
         newValue = watchFn(scope);
 
         if (_.isObject(newValue)) {
@@ -293,22 +295,32 @@ Scope.prototype.$watchCollection = function (watchFn, listenerFn) {
                 if (!_.isObject(oldValue) || _.isArrayLike(oldValue)) {
                     changeCount++;
                     oldValue = {};
+                    oldLength = 0;
                 }
+                newLength = 0;
                 _.forOwn(newValue, function (newVal, key) {
-                    var bothNaN = _.isNaN(newVal) && _.isNaN(oldValue[key]);
-
-                    if (!bothNaN && oldValue[key] !== newVal) {
+                    newLength++;
+                    if (oldValue.hasOwnProperty(key)) {
+                        var bothNaN = _.isNaN(newVal) && _.isNaN(oldValue[key]);
+                        if (!bothNaN && oldValue[key] !== newVal) {
+                            changeCount++;
+                            oldValue[key] = newVal;
+                        }
+                    } else {
                         changeCount++;
+                        oldLength++;
                         oldValue[key] = newVal;
                     }
                 });
-
-                _.forOwn(oldValue, function (oldVal, key) {
-                    if (!newValue.hasOwnProperty(key)) {
-                        changeCount++;
-                        delete oldValue[key];
-                    }
-                });
+                if (oldLength > newLength) {
+                    changeCount++;
+                    _.forOwn(oldValue, function (oldVal, key) {
+                        if (!newValue.hasOwnProperty(key)) {
+                            oldLength--;
+                            delete oldValue[key];
+                        }
+                    });
+                }
             }
         } else {
 

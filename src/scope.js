df16c2dd5ad0f967c85784bcc7ea55184749b618
RathaR <ivan.pecharitsa@gmail.com>
2015-04-22 00:16:54 +0300

    added detecting for new or replaced attributes in Objects

--- src/scope.js
+++ src/scope.js
@@ -294,6 +294,14 @@ Scope.prototype.$watchCollection = function (watchFn, listenerFn) {
                     changeCount++;
                     oldValue = {};
                 }
+                _.forOwn(newValue, function(newVal, key) {
+                    var bothNaN = _.isNaN(newVal) && _.isNaN(oldValue[key]);
+
+                    if(!bothNaN && oldValue[key] !== newVal) {
+                        changeCount++;
+                        oldValue[key] = newVal;
+                    }
+                });
             }
         } else {
 

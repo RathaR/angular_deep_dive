f6e27c588863dbf55482dd733ece76dabb19eddd
RathaR <ivan.pecharitsa@gmail.com>
2015-04-23 22:54:30 +0300

    parsing numbers in scientific notation

--- src/parse.js
+++ src/parse.js
@@ -32,7 +32,8 @@ Lexer.prototype.lex = function (text) {
     while (this.index < this.text.length) {
         this.ch = this.text.charAt(this.index);
 
-        if (this.isNumber(this.ch)) {
+        if (this.isNumber(this.ch) ||
+            (this.ch === '.' && this.isNumber(this.peek()))) {
             this.readNumber();
         } else {
             throw 'Unexpected next character ' + this.ch;
@@ -45,14 +46,34 @@ Lexer.prototype.isNumber = function (ch) {
     return '0' <= ch && ch <= '9';
 };
 
+Lexer.prototype.peek = function () {
+    return this.index < this.text.length - 1 ?
+        this.text.charAt(this.index + 1) :
+        false;
+};
+
+Lexer.prototype.isExpOperator = function (ch) {
+    return ch === '-' || ch === '+' || this.isNumber(ch);
+};
+
 Lexer.prototype.readNumber = function () {
     var number = '';
     while (this.index < this.text.length) {
-        var ch = this.text.charAt(this.index);
-        if (this.isNumber(ch)) {
+        var ch = this.text.charAt(this.index).toLowerCase();
+        if (ch === '.' || this.isNumber(ch)) {
             number += ch;
         } else {
-            break;
+            var nextCh = this.peek();
+            var prevCh = number.charAt(number.length - 1);
+            if (ch === 'e' && this.isExpOperator(nextCh)) {
+                number += ch;
+            } else if (this.isExpOperator(ch) && prevCh === 'e' && nextCh && this.isNumber(nextCh)) {
+                number += ch;
+            } else if (this.isExpOperator(ch) && prevCh === 'e' && (!nextCh || !this.isNumber(nextCh))) {
+                throw 'Invalid exponent';
+            } else {
+                break;
+            }
         }
         this.index++;
     }

5b9c210edbc1990ccc8b367673eeb7abe273519b
RathaR <ivan.pecharitsa@gmail.com>
2015-04-30 22:14:29 +0300

    dependency injection

--- src/injector.js
+++ src/injector.js
@@ -14,6 +14,13 @@ function createInjector(modulesToLoad) {
         }
     };
 
+    function invoke(fn) {
+        var args = _.map(fn.$inject, function (token) {
+            return cache[token];
+        });
+        return fn.apply(null, args);
+    }
+
     _.forEach(modulesToLoad, function loadModule(moduleName) {
         if (!loadedModules.hasOwnProperty(moduleName)) {
             loadedModules[moduleName] = true;
@@ -33,6 +40,7 @@ function createInjector(modulesToLoad) {
         },
         get: function (key) {
             return cache[key];
-        }
+        },
+        invoke: invoke
     };
 }

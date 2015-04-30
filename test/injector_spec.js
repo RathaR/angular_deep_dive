f88194a24c626ef5507cae83fa9296bbc9c43f7e
RathaR <ivan.pecharitsa@gmail.com>
2015-04-30 22:19:41 +0300

    reject non-string DI tokens

--- test/injector_spec.js
+++ test/injector_spec.js
@@ -102,4 +102,20 @@ describe('injector', function () {
         expect(injector.invoke(fn)).toBe(3);
     });
 
+    it('does not accept non-string as injection tokens', function () {
+        var module = angular.module('myModule', []);
+        module.constant('a', 1);
+        var injector = createInjector(['myModule']);
+
+        var fn = function (one, two) {
+            return one + two;
+        };
+        fn.$inject = ['a', 2];
+
+        expect(function() {
+            injector.invoke(fn);
+        }).toThrow();
+    });
+
+
 });

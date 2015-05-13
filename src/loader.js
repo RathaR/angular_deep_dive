8afddfe66b934de7124d6e202edd356d17533b0e
RathaR <ivan.pecharitsa@gmail.com>
2015-05-13 21:24:17 +0300

    factories

--- src/loader.js
+++ src/loader.js
@@ -31,6 +31,7 @@ function setupModuleLoader(window) {
             provider: invokeLater('$provide', 'provider'),
             config: invokeLater('$injector', 'invoke', 'push', configBlocks),
             factory: invokeLater('$provide', 'factory'),
+            value: invokeLater('$provide', 'value'),
             run: function (fn) {
                 moduleInstance._runBlocks.push(fn);
                 return moduleInstance;

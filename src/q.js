faf242594db08ec583d7b9fae8662345e97a9240
RathaR <ivan.pecharitsa@gmail.com>
2015-05-16 13:09:18 +0300

    creating deferreds

--- src/q.js
+++ src/q.js
@@ -4,5 +4,16 @@
 function $QProvider() {
     this.$get = function () {
 
+        function Deferred() {
+
+        }
+
+        function defer() {
+            return new Deferred();
+        }
+
+        return {
+            defer: defer
+        }
     };
 }
